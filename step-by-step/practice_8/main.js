a8d52be136c519df8e91454bfd7b4da5be18e48d
邱雪 <1310265832@qq.com>
2018-08-29 19:42:01 +0800

    add more oop practice

--- step-by-step/practice_8/main.js
+++ step-by-step/practice_8/main.js
@@ -14,10 +14,16 @@ function Student(name,age,class_) {
     this.property = 'Student';
 }
 
+class Class_ {
+    constructor(number){
+        this.number = number;
+    }
+}
 Student.prototype = new Person();
 Student.prototype.introduce = function () {
-    return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I am at Class ${this.class_}.`;
+    return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I am at Class ${this.class_.number}.`;
 };
+
 
 function Teacher(name,age,class_) {
     this.name = name;
@@ -38,14 +44,30 @@ Teacher.prototype.introduce = function () {
     return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I teach ${teachClass}.`;
 };
 
+Teacher.prototype.introduceWith = function (student) {
+    if(student.class_.number === this.class_){
+        return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I teach ${student.name}.`;
+    }else {
+        return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I don't teach ${student.name}.`;
+    }
+
+
+};
+
 let person = new Person('Tom',21);
 console.log(person.introduce());
 
-let student = new Student('Tom',22,3);
+let studentClass = new Class_(4);
+let student = new Student('Tom',22,studentClass);
 console.log(student.introduce());
 
 let teacher = new Teacher('amy',34,3);
 console.log(teacher.introduce());
+console.log(teacher.introduceWith(student));
 
 let teacher2 = new Teacher('amy',34);
 console.log(teacher2.introduce());
+
+let teacher3 = new Teacher('amy',34,4);
+console.log(teacher3.introduce());
+console.log(teacher3.introduceWith(student));
--- step-by-step/practice_9/main.js
+++ step-by-step/practice_9/main.js
@@ -1,4 +1,5 @@
-function Person(name,age) {
+function Person(id,name,age) {
+    this.id = id;
     this.name = name;
     this.age = age;
 }
@@ -7,17 +8,29 @@ Person.prototype.introduce = function () {
     return `My name is ${this.name}. I am ${this.age} years old.`;
 };
 
-function Student(name,age,class_) {
+function Student(id,name,age,class_) {
+    this.id = id;
     this.name = name;
     this.age = age;
     this.class_ = class_;
     this.property = 'Student';
 }
 
+class Class_ {
+    constructor(number){
+        this.number = number;
+        this.leader = leader;
+    }
+
+    assignLeader(student){
+
+    }
+}
 Student.prototype = new Person();
 Student.prototype.introduce = function () {
-    return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I am at Class ${this.class_}.`;
+    return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I am at Class ${this.class_.number}.`;
 };
+
 
 function Teacher(name,age,class_) {
     this.name = name;
@@ -38,14 +51,30 @@ Teacher.prototype.introduce = function () {
     return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I teach ${teachClass}.`;
 };
 
+Teacher.prototype.introduceWith = function (student) {
+    if(student.class_.number === this.class_){
+        return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I teach ${student.name}.`;
+    }else {
+        return new Person(this.name,this.age).introduce()+`I am a ${this.property}. I don't teach ${student.name}.`;
+    }
+
+
+};
+
 let person = new Person('Tom',21);
 console.log(person.introduce());
 
-let student = new Student('Tom',22,3);
+let studentClass = new Class_(4);
+let student = new Student('Tom',22,studentClass);
 console.log(student.introduce());
 
 let teacher = new Teacher('amy',34,3);
 console.log(teacher.introduce());
+console.log(teacher.introduceWith(student));
 
 let teacher2 = new Teacher('amy',34);
 console.log(teacher2.introduce());
+
+let teacher3 = new Teacher('amy',34,4);
+console.log(teacher3.introduce());
+console.log(teacher3.introduceWith(student));
